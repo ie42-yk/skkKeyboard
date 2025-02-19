aaead39c908e80e155843dbdf33e2ba0d81da4e9
ie42-yk <e215742@ie.u-ryukyu.ac.jp>
2025-02-19 22:21:57 +0900

    Update

--- app/src/main/java/com/example/skkkeyboard/MainActivity.kt
+++ app/src/main/java/com/example/skkkeyboard/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.skkkeyboard
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.Settings
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.skkkeyboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private fun isKeyboardEnabled(): Boolean {
+        val inputmethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val enableInputMethods = inputmethodManager.enabledInputMethodList.map { it.id }
+        return enableInputMethods.contains("com.example.skkkeyboard/.InputMethodService")
+    }
+
+    private fun openKeyboardCooserSettings() {
+        val im = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        im.showInputMethodPicker()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+              // Check keyboard status
+              if (isKeyboardEnabled())
+                  btnEnableKeyboard.isEnabled = false
+              btnEnableKeyboard.setOnClickListener {
+                  if (!isKeyboardEnabled())
+                    openKeyboardCooserSettings()
+              }
+              btnCooseKeyboard.setOnClickListener {
+                  if (isKeyboardEnabled())
+                      openKeyboardCooserSettings()
+                  else Toast.makeText(
+                    this@MainActivity,
+                    "Coose the keyboard activation button",
+                    Toast.LENGTH_SHORT
+                  ).show()
+              }
         }
+    }
+    private fun openKeyboardSettings() {
+        val intent = Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)
+        startActivity(intent)
     }
 }
